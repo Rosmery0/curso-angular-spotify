bcb48c8aa18e0f8e414256a2d63102d07d58d0b1
Rosmery0 <rosmery.r@outlook.es>
2022-07-21 17:57:42 -0400

    Secod version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spotify';
+  name = 'Rosmery Rosario';
+  txtname = "Type your name here!!!";
+  disabled = true;
+  img = "https://cdnb.artstation.com/p/assets/covers/images/045/154/127/large/thibaut-granet-thibaut-granet-template-artstation-jinx.jpg?1642036430"
+  
+  text = "This is a event binding example";
+  text2:string = "";
+  constructor(){
+    setInterval(() => this.disabled=false, 3000);
+
+  }
+
+  getSum(num1:number, num2:number){
+    return num1 + num2;
+  }
+
+  changeText(): void{
+    this.text = "The text has changed!!!";
+  }
 }
